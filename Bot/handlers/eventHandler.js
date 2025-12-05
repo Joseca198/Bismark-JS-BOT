fc2473d649fa78cce6ee9b9165d5a8d30c9b6980
Joseca198 <joseca198gameryt@gmail.com>
2025-12-05 01:41:17 -0600

    Terminado Handler Events
    Termina el obtenedor de eventos que registra todos los eventos que puede tener el bot de discord

--- Bot/handlers/eventHandler.js
+++ Bot/handlers/eventHandler.js
@@ -1,3 +1,29 @@
+// ------ Event Handler ------
+// Obtiene todos los eventos de la carpeta de eventos y los ejecuta.
+// ------
+
+const path =  require("path");
+const getAllFiles = require("../utils/getAllFiles")
+
 module.exports = (client) => {
-    
+
+    const eventFolders = getAllFiles(path.join(__dirname, "..", "events"),true);
+
+    for (const eventFolder of eventFolders){
+        const eventFiles = getAllFiles(eventFolder);
+        eventFiles.sort((a , b) => a>b);
+        console.log(eventFiles)
+        
+        const eventName = eventFolder.replace(/\\/g , "/").split("/").pop();
+
+        client.on(eventName, async (arg) => {
+            for (const eventFile of eventFiles) {
+                const eventFunction = require(eventFile);
+                await eventFunction(client, arg);
+            }
+        });
+
+    }
+
+
 };
