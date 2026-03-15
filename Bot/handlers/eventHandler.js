08c05090bc0721b0872943dc0045ea9c5da450ad
Joseca198 <joseca198gameryt@gmail.com>
2026-03-15 13:17:24 -0600

    Arreglando detalles
    Se arreglaron detalles de bismark, en este caso se cambio el nombre de la carpeta de Bismarck.js a Index.js, esto para tener mejor la estructura, pero tambien para que cuando tenga que vender el bot no le tenga que cambiar el nombre a estos archivos.

--- Bot/handlers/eventHandler.js
+++ Bot/handlers/eventHandler.js
@@ -7,11 +7,11 @@ const getAllFiles = require("../utils/getAllFiles")
 
 module.exports = (client) => {
 
-    const eventFolders = getAllFiles(path.join(__dirname, "..", "events"),true);
+    const eventFolders = getAllFiles(path.join(__dirname, "..", "events"), true);
 
     for (const eventFolder of eventFolders){
-        const eventFiles = getAllFiles(eventFolder);
-        eventFiles.sort((a , b) => a>b);
+        let eventFiles = getAllFiles(eventFolder);
+        eventFiles = eventFiles.sort();
         
         const eventName = eventFolder.replace(/\\/g , "/").split("/").pop();
 
